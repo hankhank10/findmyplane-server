31d373fc6ab7b14bf7f3ee77f654f5dc2b828c70
hankhank10 <m.a.hankinson@gmail.com>
2021-01-24 17:16:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -487,13 +487,13 @@ def latest_client_check():
 @app.route('/download/findmyplane-setup.exe')
 def download_setup_link():
     stats_handler.increment_stat('downloads')
-    redirect("https://github.com/hankhank10/findmyplane-client/releases/download/v0.7/findmyplane-setup.exe")
+    return redirect('https://github.com/hankhank10/findmyplane-client/releases/download/v0.7/findmyplane-setup.exe')
 
 
 @app.route('/download/findmyplane-client.zip')
 def download_zip_link():
     stats_handler.increment_stat('downloads')
-    redirect("https://github.com/hankhank10/findmyplane-client/releases/download/v0.7/findmyplane-client-7.zip")
+    return redirect("https://github.com/hankhank10/findmyplane-client/releases/download/v0.7/findmyplane-client-7.zip")
 
 
 @app.route('/debug_sentry')
