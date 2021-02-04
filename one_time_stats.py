7928ad77818d73a704bfd1bab04d1294f31dfc09
hankhank10 <m.a.hankinson@gmail.com>
2021-02-04 19:14:04 +0000

    Update one_time_stats.py

--- one_time_stats.py
+++ one_time_stats.py
@@ -1,6 +1,6 @@
 import stats_handler2
 
-for x in range(1, 649):
-    stats_handler2.log_event('page_load')
+#for x in range(1, 649):
+#    stats_handler2.log_event('page_load')
 
 stats_handler2.create_database()
