2e58038d4f17c4d73b236165b37c2497951a9bf0
Mark Hank <mark@secureup.link>
2021-03-13 16:57:53 +0000

    Remove track on just_map mode

---
+++ punctuate.py
@@ -0,0 +1,5 @@
+from fastpunct import FastPunct
+fastpunct = FastPunct()
+output = fastpunct.punct("ladies and gentlemen this is your captain speaking it is time to begin our descent")
+
+print (output)
